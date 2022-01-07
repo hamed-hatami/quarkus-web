493d776af85de5d50086d07625cb085619cd9f88
Hamed Hatami <hamed.hatami@ericsson.com>
2022-01-07 16:30:38 +0100

    add some curl command to test

--- src/main/java/org/hamed/quarkus/api/CarResource.java
+++ src/main/java/org/hamed/quarkus/api/CarResource.java
@@ -13,7 +13,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/car")
+@Path("/api/car")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class CarResource {
--- src/main/java/org/hamed/quarkus/model/repository/CarRepository.java
+++ src/main/java/org/hamed/quarkus/model/repository/CarRepository.java
@@ -5,12 +5,14 @@ import org.hamed.quarkus.model.entity.Car;
 import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 @ApplicationScoped
 public class CarRepository implements PanacheRepository<Car> {
 
+    @Transactional
     public void createCar(Car car) {
         car.persist();
     }
